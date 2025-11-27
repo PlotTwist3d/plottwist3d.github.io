e4e35a648be9b0e9e22f67cfacb3a76e805f6a3e
DanielaNC <daniela.narciso@hotmail.com>
2025-11-27 20:08:29 +0000

    ch3 update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'the-phantom-agent-2';
+var cacheName = 'the-phantom-agent-ride-or-die';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
